3a01f5ab8c7f4f140c0d7dc14a760d7009050f6b
Shivam Kumar <143876488+Shivam-Kumar-Chauhan@users.noreply.github.com>
2024-09-09 00:41:00 +0530

    Add files via upload

---
+++ Functions/HelloWorldUsingFunction/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        printHelloWorld(); //--> Function Call
+    }
+    public static void printHelloWorld(){
+        System.out.println("Hello Wolrd");
+    }
+}
