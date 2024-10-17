80237eb3342b2c7b783c93d7f91b10ccd3d3c44d
Shivam Kumar <143876488+Shivam-Kumar-Chauhan@users.noreply.github.com>
2024-10-17 07:50:16 +0530

    Delete Easy(Introduction) directory

--- Easy(Introduction)/sumoftwonumber/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main{
-    public static void main(String[] args) {
-        int a = 28;
-        int b = 12;
-        int c;
-        c = a + b;
-        System.out.println(c);
-    }
-}
